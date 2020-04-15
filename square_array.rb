b3906d2d6fd312b6e2c3840a7dda0e16e2a80a6b
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-15 18:38:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 
 def square_array(array)
+  numbers = [1,2,3]
   new numbers = []
-  array.each do |number| new
-    puts number**2 
-  end
+  array.each do |number| new array << number**2 end
+    new numbers
 end
