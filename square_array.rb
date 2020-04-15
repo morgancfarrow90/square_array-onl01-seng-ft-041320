8ab35a3ae4c55faab6618f4ecef7f69ff536a231
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-15 18:49:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,5 @@
-def square_array(array)
-  new numbers = []
-  array.each do || 
-    new array << number**2 
-  end
-    new numbers
-end
+new numbers = []
 
-numbers = [1,2,3]
-square_array(numbers)
+def square_array(array)
+  array.each do |integer|
+    
